32fb806b3985334831b245708366aa22815764ee
goyounha11 <goyounha11@naver.com>
2020-09-26 19:48:40 +0900

    ㅎㅎ

--- src/components/Business.js
+++ src/components/Business.js
@@ -6,6 +6,7 @@ function Business(){
         <div className="contentArea">
             <h1>비지니스소개페이지</h1>
             <h1>비지니스소개페이지</h1>
+            <h1>진태야.. 나도 왜 되는지 잘 몰라....;;</h1>
         </div>
     )
 }
