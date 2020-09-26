38ee0ddead3cb2534b76f7e8021b839a37a00d6b
goyounha12 <tlscjfgh93@gmail.com>
2020-09-26 19:09:47 +0900

    test2

--- src/components/Team.js
+++ src/components/Team.js
@@ -14,6 +14,7 @@ function Team(){
                 <div className="team_1"></div>
                 <div className="team_1"></div>
                 <div className="team_1"></div>
+                <div>gkdl</div>
             </div>
         </div>
     )
