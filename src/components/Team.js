bd5c1ba9569bacb7ab2f4b8e53abcb00430122f9
goyounha11 <goyounha11@naver.com>
2020-09-27 17:05:06 +0900

    Add Teamhover

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,8 +1,6 @@
 import React from "react";
 import "./Team.css";
-import Mainimg from "../image/Team/main.gif";
-import p1 from "../image/Team/p1.gif";
-
+import {ReactComponent as Team_main} from './Team_main.svg';
 function Team() {
     return (
         <div className="contentArea">
@@ -10,16 +8,8 @@ function Team() {
                 <h1>Team</h1>
             </div>
             <div className="team_main">
-                <img src={Mainimg} alt="bg" useMap="#Map1" />
-                <map name="Map1" id="Map">
-                    <area href="javascript:(function(){document.getElementById('img1').src={p1};})()" shape="circle" coords="240,180,140"/>
-                    <area href="javascript:(function(){document.getElementById('img1').src='https://www.xul.fr/images/collar.jpg';})()" shape="rect" coords="60,330,440,520"/>
-                </map>
+                <Team_main />
             </div>
-
-                    <div class="selection">
-                        <p>click a brick</p>
-                    </div>
         </div >
     )
 }
