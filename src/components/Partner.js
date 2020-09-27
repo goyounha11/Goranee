369b20383653ded000fd721c6f2183b34e4fab15
amy1289 <amy1289@naver.com>
2020-09-27 12:32:15 +0900

    Partner 추가

--- src/components/Partner.js
+++ src/components/Partner.js
@@ -1,10 +1,24 @@
 import React from "react";
-
+import "./Partner.css"
 function Partner(){
     return(
+        <div>
+        <div className="titleArea">
+            <p className="title">Partner</p>
+            <p className="intro">의뢰주신 고객사를 소개합니다.</p>
+        </div>
+
         <div className="contentArea">
-            <h1>파트너소개페이지</h1>
-            <h1>원격해달라하고 어딘가 사라지는 지또지 ㅋㅋㅋㅋㅋㅋㅋㅋㅋ</h1>
+            <div class="partner-container">
+            <img src="http://placeimg.com/140/45/1" class="partner-item"></img>
+            <img src="http://placeimg.com/140/45/1" class="partner-item"></img>
+            <img src="http://placeimg.com/140/45/1" class="partner-item"></img>
+            <img src="http://placeimg.com/140/45/1" class="partner-item"></img>
+            <img src="http://placeimg.com/140/45/1" class="partner-item"></img>
+            <img src="http://placeimg.com/140/45/1" class="partner-item"></img>
+            <img src="http://placeimg.com/140/45/1" class="partner-item"></img>
+            </div>
+        </div>
         </div>
     )
 }
