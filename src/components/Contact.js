3f36f678e89a666879bc1151c72d3f932392bf20
tjk1150 <tjk1150@kakao.com>
2020-09-29 00:55:30 +0900

    jintae

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,21 +3,9 @@ import React from "react";
 function Contact(){
     return(
         <div className="contentArea">
-            <div className="container">
-                <div className="row">
-                    <div className="business_titleArea">
-                        <p className="title">Contact US</p>
-                    </div>
-                    <div className="business_contentArea">
-                        <div className="one_content_LetterArea">
-                            <p>서울본사</p>
-                            ADDRESS
-                            서울시 서울시 서초구 서초대로 396 강남빌딩 16F 1605호
-                        </div>
-                        <div className="">
-                            <img src="http://placeimg.com/450/327/1" alt="Business content"/>
-                        </div>
-                    </div>
+            <div className="business_container">
+                <div className="business_titleArea">
+                    <p className="title">Contect Us</p>
                 </div>
             </div>
         </div>
