578656831121f6d15c978d77157a492aca54c8ff
tjk1150 <tjk1150@kakao.com>
2020-09-28 11:16:01 +0900

    jintae

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,9 +3,9 @@ import "./Home.css";
 
 function Home(){
     return (
-        <div className="contentArea"> 
+        <div className="main_contentArea"> 
         <div className="main_visual"></div>
-        <div className="sub_visual">
+        <div className="contentArea">
             <div className="sub_1"></div>
             <div className="sub_2"></div>
             <div className="sub_3"></div>
