3f3dac9ceb4168d975b802589b734f04ec52e096
tjk1150 <tjk1150@kakao.com>
2020-09-28 17:01:10 +0900

    jintae_business

--- src/components/Business.js
+++ src/components/Business.js
@@ -4,39 +4,35 @@ import "./Business.css";
 function Business(){
     return(
         <div className="contentArea">
-            <div className="container">
+            <div className="business_container">
                 <div className="business_titleArea">
                     <p className="title">Business</p>
                 </div>
-                <div className="business_contentArea business_contentArea_a">
-                    <div className="bcLetterArea bcLetterArea_left">
-                        <p className="bcTitle bcTitle_left">
-                            재고관리 프로그램 및<br /> 견적서 작성 외주 프로그램 개발
-                        </p>
-                        <p className="bcInfo bcInfo_left">
-                            재고관리 프로그램 및 견적서 작성 외주 프로그램 <br />
-                            개발에 관한 설명이 들어가는 공간입니다.
-                        </p>
-                    </div>
-                    <div className="bcImgArea bcImgArea_right">
-                        <img src="http://placeimg.com/450/327/1" alt="Business content"/>
-                    </div>
+                <div className="bitem bitem1">
+                    <p className="bctitle">
+                        재고관리 프로그램 및<br /> 견적서 작성 외주 프로그램 개발
+                    </p>
+                    <p className="bcinfo">
+                        재고관리 프로그램 및 견적서 작성 외주 프로그램 <br />
+                        개발에 관한 설명이 들어가는 공간입니다.
+                    </p>
                 </div>
-                <div className="business_contentArea business_contentArea_b">
-                    <div className="bcImgArea bcImgArea_left">
-                        <img src="http://placeimg.com/450/327/2" alt="Business content"/>
-                    </div>
-                    <div className="bcLetterArea bcLetterArea_right">
-                        <p className="bcTitle bcTitle_right">
-                            IoT App 외주 개발</p>
-                        <p className="bcInfo bcInfo_right">
-                            IoT App 외주 개발에 관한 <br />
-                            설명이 들어가는 공간입니다.
-                        </p>
-                    </div>
-                    <div className="inner-box">
+                <div className="bitem bitem2">
+                    <img src="http://placeimg.com/450/327/1" alt="Business content"/>
+                </div>
+                <div className="bitem bitem3">
+                    <img src="http://placeimg.com/450/327/2" alt="Business content"/>
+                </div>
+                <div className="bitem bitem4">
+                    <p className="bctitle">
+                        IoT App 외주 개발</p>
+                    <p className="bcinfo">
+                        IoT App 외주 개발에 관한 <br />
+                        설명이 들어가는 공간입니다.
+                    </p>
+                </div>
+                <div className="inner-box">
 
-                    </div>
                 </div>
             </div>
         </div>
