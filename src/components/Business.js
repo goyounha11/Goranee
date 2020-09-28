f58f3d67e0b4f5b0f796d61dbd31b75b8e7545db
tjk1150 <tjk1150@kakao.com>
2020-09-28 12:26:29 +0900

    jintae

--- src/components/Business.js
+++ src/components/Business.js
@@ -10,27 +10,27 @@ function Business(){
                         <p className="title">Business</p>
                     </div>
                     <div className="business_contentArea">
-                        <div className="one_content_LetterArea">
-                            <p className="one_content_title">
+                        <div className="bcLetterArea bcLetterArea_left">
+                            <p className="bcTitle bcTitle_left">
                                 재고관리 프로그램 및<br /> 견적서 작성 외주 프로그램 개발
                             </p>
-                            <p className="one_content_info">
+                            <p className="bcInfo bcInfo_left">
                                 재고관리 프로그램 및 견적서 작성 외주 프로그램 <br />
                                 개발에 관한 설명이 들어가는 공간입니다.
                             </p>
                         </div>
-                        <div className="one_content_imgArea">
+                        <div className="bcImgArea bcImgArea_right">
                             <img src="http://placeimg.com/450/327/1" alt="Business content"/>
                         </div>
                     </div>
                     <div className="business_contentArea">
-                        <div className="two_content_imgArea">
+                        <div className="bcImgArea bcImgArea_left">
                             <img src="http://placeimg.com/450/327/2" alt="Business content"/>
                         </div>
-                        <div className="two_content_LetterArea">
-                            <p className="two_content_title">
+                        <div className="bcLetterArea bcLetterArea_right">
+                            <p className="bcTitle bcTitle_right">
                                 IoT App 외주 개발</p>
-                            <p className="two_content_info">
+                            <p className="bcInfo bcInfo_right">
                                 IoT App 외주 개발에 관한 <br />
                                 설명이 들어가는 공간입니다.
                             </p>
